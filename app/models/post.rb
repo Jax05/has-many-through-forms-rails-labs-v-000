bed93526300f4850a569c7c14b0a13d77a8c9deb
Carley Tripp <carley.tripp@outlook.com>
2019-03-05 01:35:18 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,23 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  # accepts_nested_attributes_for :categories
 
+  def categories_attributes=(category_attributes)
+    category_attributes.values.each do |category_attribute|
+      if category_attribute[:name].present?
+        category = Category.find_or_create_by(category_attribute)
+        if !self.categories.include?(category)
+          self.categories << category
+        end
+      end
+    end
+  end
 
+  # def users_attributes=(user_attribute)
+  #   user_attribute.values.each do |username|
+  #     user = User.find_or_create_by(username)
+  #     self.users << user
+  #   end
+  # end
 end
